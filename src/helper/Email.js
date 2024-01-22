0daa000c2e9d4e64b53a4ede36a8c0f208518d78
Ritika <ritikakri3708@gmail.com>
2024-01-22 19:34:30 +0530

    cred

--- src/helper/Email.js
+++ src/helper/Email.js
@@ -1,31 +1,29 @@
-const nodemailer = require("nodemailer")
-exports.sendEmail=async(req,res)=>{
-    try {
-        const transport= nodemailer.createTransport({
-            service:"gmail",
-            host:"smtp.gmail.com",
-            port:465,
-            auth:{
-                user:"voteonclick@gmail.com",
-                pass:"qkax wqop umei nnvm"
-            }
-        })
-        const data= {
-            from:"voteonclick@gmail.com",
-            to:req.body.email,
-            subject:req.subject,
-            text: req.text
-        }
-        transport.sendMail(data, (error,info)=>{
-            if(error){
-                console.log(error);
-                res.status(400).json({message:"Email Delivery Error"})
-            }else{
-                console.log(info);
-                res.status(201).json({message:"sucess"})
-            }
-        })
-    } catch (error) {
-        
-    }
-}
+const nodemailer = require("nodemailer");
+const { emailConfig } = require("../../config/config");
+
+
+exports.sendEmail = async (req, res) => {
+  try {
+    const transport = nodemailer.createTransport(emailConfig);
+    const data = {
+      from: "voteonclick@gmail.com",
+      to: req.body.email,
+      subject: req.subject,
+      text: req.text,
+    };
+
+    transport.sendMail(data, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ message: "Email Delivery Error" });
+      } else {
+        console.log(info);
+        res.status(201).json({ message: "Success" });
+      }
+    });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
