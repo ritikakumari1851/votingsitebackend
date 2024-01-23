e778bc480a2bb81f9749bb13887b310a613c2e14
Ritika <ritikakri3708@gmail.com>
2024-01-23 13:32:30 +0530

    Changes

--- index.js
+++ index.js
@@ -1,69 +1,51 @@
 // index,js
 const express = require('express');
 const server = express();
-const cors = require("cors");
 const {Server} = require("socket.io")
 const http = require("http")
-const app = http.createServer(server)
-const io = new Server(app)
-server.use(express.json());
-server.use(cors())
+const app= http.createServer(server)
+const io= new Server(app)
+const port = process.env.PORT || 3000
 
+const cors = require("cors");
 const mongoose = require('mongoose');
 const { register, login, findUser } = require('./src/Controllers/auth');
 const { verifyToken, validateForm, isValidated } = require('./src/Middleware');
 const { addForm } = require('./src/Controllers/form');
 const { sendEmail } = require('./src/helper/Email');
-
-
+server.use(express.json());
+server.use(cors())
 server.get("/", (req, res) => {
     res.status(200).json({
-        uname: "Anshu",
+        uname: "Ritika",
         uphone: "0000000",
-        username: "welcome Anshu"
+        username: "welcome Ritika"
     })
 })
+
 require('dotenv').config()
 server.post("/register",register,sendEmail)
 server.get("/get-user",verifyToken,findUser)
 server.post("/login",login)
 server.post("/addform",validateForm,isValidated,addForm,sendEmail)
-
-io.on("connection",socket=>{
+io.on("connection",(socket)=>{
     console.log("new user connected")
     socket.on("message",(message,room)=>{
-        console.log(`New message received in ${room} and message is ${message}`);
-
+        console.log`(New message recieved in ${room} and message is ${message})`;
     socket.to(room).emit("message",message)
-    
-    })
-    socket.on("join",(room)=>{
-        console.log(room);
-        socket.join(room)
-        socket.emit("joined")
-    })
-    })
-    socket.on("disconnect", (reason, details) => {
-        // the reason of the disconnection, for example "transport error"
-        console.log(reason);
-      
-        // the low-level reason of the disconnection, for example "xhr post error"
-        console.log(details.message);
-      
-        // some additional description, for example the status code of the HTTP response
-        console.log(details.description);
-      
-        // some additional context, for example the XMLHttpRequest object
-        console.log(details.context);
-      });
-
-    const port = process.env.PORT || 4000;
+})
+socket.on("join",(room)=>{
+    socket.join(room)
+    socket.emit("joined")
+})
+})
+        
 mongoose.connect(
    process.env.MONGO_URL
 )
     .then(() => console.log("Database is connected"))
     .catch(error => console.log(error.message));
 
-server.listen(port, function () {
+app.listen(port, function () {
     console.log("Server is on");
 });
