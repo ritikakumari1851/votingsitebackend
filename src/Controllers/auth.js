18c3c7c725bdc265c1686b02c5d5a15f8cfdbe78
Ritika <ritikakri3708@gmail.com>
2024-02-14 09:45:42 +0530

    4

--- src/Controllers/auth.js
+++ src/Controllers/auth.js
@@ -188,8 +188,8 @@ exports.vote = async (req, res) => {
     await existingVoter.save();
 
     return res.status(200).json({ message: "Vote submitted successfully" });
-  } catch (error) {
+  }catch (error) {
     console.error("Error submitting vote:", error);
-    return res.status(500).json({ message: "Internal server error", error: error.message });
+    return res.status(500).json({ message: "Internal server error", error: error.toString() });
 }
 };
